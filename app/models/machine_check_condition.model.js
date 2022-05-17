2e7232e6fbe261ee14bf2bbd7651a3edef0b1760
Andi Galuh <andy13galuh@gmail.com>
2022-05-17 11:21:31 +0700

    comment on check machine

--- app/models/machine_check_condition.model.js
+++ app/models/machine_check_condition.model.js
@@ -2,7 +2,12 @@ module.exports = (sequelize, Sequelize) => {
   const Machine_check_condition = sequelize.define("machine_check_condition", {
     status: {
       type: Sequelize.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
+    },
+    comment_value: {
+      type: Sequelize.TEXT,
+      defaultValue: false,
+      allowNull: false,
     },
   });
 
